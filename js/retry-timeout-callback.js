a1b03bd0a6c5ea69d891304b2192501f1dbdb5d1
Kevin Lau <laukevin@us.ibm.com>
2018-03-26 10:00:17 -0500

    Add MP Fault Tolerance Feature step.

--- js/retry-timeout-callback.js
+++ js/retry-timeout-callback.js
@@ -10,4 +10,174 @@
 *******************************************************************************/
 var retryTimeoutCallback = (function() {
 
+    var listenToEditorForFeatureInServerXML = function(editor) {
+        var saveServerXML = function(editor) {
+            __saveServerXML(editor);
+        };
+        editor.addSaveListener(saveServerXML);
+    };
+
+    var addMicroProfileFaultToleranceFeatureButton = function(event) {
+        if (event.type === "click" ||
+           (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
+            // Click or 'Enter' or 'Space' key event...
+            __addMicroProfileFaultToleranceFeature();
+        }
+    };
+
+    var __addMicroProfileFaultToleranceFeature = function() {
+        var FTFeature = "      <feature>mpFaultTolerance-1.0</feature>";
+        var stepName = stepContent.getCurrentStepName();
+        var serverFileName = "server.xml";
+
+        // reset content every time annotation is added through the button so as to clear out any
+        // manual editing
+        contentManager.resetTabbedEditorContents(stepName, serverFileName);
+        var content = contentManager.getTabbedEditorContents(stepName, serverFileName);
+
+        contentManager.insertTabbedEditorContents(stepName, serverFileName, 5, FTFeature);
+        var readOnlyLines = [];
+        // mark cdi feature line readonly
+        readOnlyLines.push({
+            from: 4,
+            to: 4
+        });
+        contentManager.markTabbedEditorReadOnlyLines(stepName, serverFileName, readOnlyLines);
+    };
+    var __getMicroProfileFaultToleranceFeatureContent = function(content) {
+        var editorContents = {};
+        try {
+            // match
+            // <featureManager>
+            //    <anything here>
+            // </featureManager>
+            // and capturing groups to get content before featureManager, the feature, and after
+            // featureManager content.
+            var featureManagerToMatch = "([\\s\\S]*)<featureManager>([\\s\\S]*)<\\/featureManager>([\\s\\S]*)";
+            var regExpToMatch = new RegExp(featureManagerToMatch, "g");
+            var groups = regExpToMatch.exec(content);
+            editorContents.beforeFeature = groups[1];
+            editorContents.features = groups[2];
+            editorContents.afterFeature = groups[3];
+        }
+        catch (e) {
+        }
+        return editorContents;
+    };
+    var __getMicroProfileFaultToleranceFeatureContent = function(content) {
+        var editorContents = {};
+        try {
+            // match
+            // <featureManager>
+            //    <anything here>
+            // </featureManager>
+            // and capturing groups to get content before featureManager, the feature, and after
+            // featureManager content.
+            var featureManagerToMatch = "([\\s\\S]*)<featureManager>([\\s\\S]*)<\\/featureManager>([\\s\\S]*)";
+            var regExpToMatch = new RegExp(featureManagerToMatch, "g");
+            var groups = regExpToMatch.exec(content);
+            editorContents.beforeFeature = groups[1];
+            editorContents.features = groups[2];
+            editorContents.afterFeature = groups[3];
+        }
+        catch (e) {
+
+        }
+        return editorContents;
+    };
+    var __isFaultToleranceInFeatures = function(features) {
+        var match = false;
+        features = features.replace('\n', '');
+        features = features.replace(/\s/g, ''); // Remove whitespace
+        try {
+            var featureMatches = features.match(/<feature>[\s\S]*?<\/feature>/g);
+            $(featureMatches).each(function (index, feature) {
+                if (feature.indexOf("<feature>mpFaultTolerance-1.0</feature>") !== -1) {
+                    match = true;
+                    return false; // break out of each loop
+                }
+            });
+        }
+        catch (e) {
+        }
+        return match;
+    };
+
+    var __isCDIInFeatures = function(features) {
+        var match = false;
+        features = features.replace('\n', '');
+        features = features.replace(/\s/g, ''); // Remove whitespace
+        try {
+            var featureMatches = features.match(/<feature>[\s\S]*?<\/feature>/g);
+            $(featureMatches).each(function (index, feature) {
+                if (feature.indexOf("<feature>cdi-1.2</feature>") !== -1) {
+                    match = true;
+                    return false; // break out of each loop
+                }
+            });
+        }
+        catch (e) {
+        }
+        return match;
+    };
+    var __checkMicroProfileFaultToleranceFeatureContent = function(content) {
+        var isFTFeatureThere = true;
+        var editorContentBreakdown = __getMicroProfileFaultToleranceFeatureContent(content);
+        if (editorContentBreakdown.hasOwnProperty("features")) {
+            isFTFeatureThere =  __isFaultToleranceInFeatures(editorContentBreakdown.features) &&
+                                __isCDIInFeatures(editorContentBreakdown.features);
+            if (isFTFeatureThere) {
+                // check for whether other stuffs are there
+                var features = editorContentBreakdown.features;
+                features = features.replace('\n', '');
+                features = features.replace(/\s/g, '');
+                if (features.length !== "<feature>mpFaultTolerance-1.0</feature><feature>cdi-1.2</feature>".length) {
+                    isFTFeatureThere = false; // contains extra text
+                }
+            }
+        } else {
+            isFTFeatureThere = false;
+        }
+        return isFTFeatureThere;
+    };
+
+    var __correctEditorError = function(stepName, isSave, fallback) {
+        // correct annotation/method
+        if (stepName === "AddLibertyMPFaultTolerance") {
+            __addMicroProfileFaultToleranceFeature();
+        }
+        // call save editor
+        if (isSave === true) {
+            __saveButtonEditor(stepName);
+        }
+    };
+
+    var __saveServerXML = function(editor) {
+        var stepName = stepContent.getCurrentStepName();
+        var serverFileName = "server.xml";
+
+        var content = contentManager.getTabbedEditorContents(stepName, serverFileName);
+        if (__checkMicroProfileFaultToleranceFeatureContent(content)) {
+            editor.closeEditorErrorBox(stepName);
+            contentManager.markCurrentInstructionComplete(stepName);
+        } else {
+            // display error to fix it
+            editor.createErrorLinkForCallBack(true, __correctEditorError);
+        }
+    };
+    var saveServerXMLButton = function(event) {
+        if (event.type === "click" ||
+           (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
+            // Click or 'Enter' or 'Space' key event...
+            contentManager.saveTabbedEditor(stepContent.getCurrentStepName(), "server.xml");
+        }
+    };
+
+    return {
+        listenToEditorForFeatureInServerXML: listenToEditorForFeatureInServerXML,
+        addMicroProfileFaultToleranceFeatureButton: addMicroProfileFaultToleranceFeatureButton,
+        addMicroProfileFaultToleranceFeature: __addMicroProfileFaultToleranceFeature,
+        saveServerXML: __saveServerXML,
+        saveServerXMLButton: saveServerXMLButton
+    }
 })();
