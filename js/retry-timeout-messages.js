01a34862a55815fb38113afad820299be4cace59
Annie Nguyen <aknguyen@us.ibm.com>
2018-05-24 16:15:31 -0500

    update text per ID review

--- js/retry-timeout-messages.js
+++ js/retry-timeout-messages.js
@@ -1,5 +1,5 @@
 var retryTimeoutMessages = {
-    OVERLAY_TEXT: "Indefinite page load. Please continue to the next step.",
+    OVERLAY_TEXT: "The page loads indefinitely. Continue to the next step.",
     TIMEOUT_REQUIRED: "@Timeout required for the playground",
     RETRY_REQUIRED: "@Retry required for the playground",
     INVALID_PARAMETER_VALUE: "Invalid parameter value",
